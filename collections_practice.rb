fcc674f48bd3c737e764be57d67067974c285162
Britny Edwards <bredwards2@gmail.com>
2018-11-25 21:30:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
 # your code goes here
+def find_cool(array)
+  array.select do |item| 
+    item.has_value?('cool')
+    # item.values == 'cool'
+  end
+end
+
+cool = [
+            {
+                   :name => "ashley",
+            :temperature => "sort of cool"
+        },
+            {
+                   :name => "blake",
+            :temperature => "cool"
+        }
+    ]
+    
+puts find_cool(cool)
